cf33578b8034e974664382077b4bca811dc2c202
ondghoskot <anaschtioui65@gmail.com>
2024-04-19 20:45:01 +0000

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create a file in /tmp using Puppet
 
 file { 'temp school file':
-  ensure => file,
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  conetnt => 'I love Puppet'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
