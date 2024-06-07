61f1a7b39ace56adc0976f185e31ca569a159df6
anassch <anaschtioui65@gmail.com>
2024-06-07 00:50:14 +0000

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns number of subscribors of a subreddit"""
-    url = f'htpps://www.reddit.com/r/{subreddit}/about.json'
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'CustomHeader/0.1'}
-    response = request.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         return response.json()['data']['subscribers']
