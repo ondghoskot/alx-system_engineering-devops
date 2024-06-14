be4642db84aa55ce079f8d4f6142f01b5bb0b4b5
anassch <anaschtioui65@gmail.com>
2024-06-14 00:50:17 +0000

    webstack debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# configure nginx to open more files
+exec { 'change ULIMIT':
+  command  => "sed -i 's/15/4096/g' /etc/default/nginx",
+  provider => shell
+}
+exec { 'nginx restart':
+  command  => "service nginx restart",
+  provider => shell
+}
